000fa8553332f7b84a67a373682920c61177733f
David Keogh <davekeogh@shaw.ca>
2015-06-17 02:08:36 -0700

    Make the main file more generic

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import sys
 from gi.repository import Gtk
 from ui import Window
 from parse import EPub
 
 
 if __name__ == '__main__':
-    lel = EPub('The Stranger - Albert Camus.epub', debug=True)
-    # lel = EPub('Jojo\'s Bizarre Adventure - Over Heaven - NisiOisin.epub', debug=True)
+    lel = EPub(sys.argv[1], debug=True)
 
     win = Window(lel)
     Gtk.main()
