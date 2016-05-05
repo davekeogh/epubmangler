b30b2f9772a6d69f69dc139b8d714dccc586bd81
David Keogh <davekeogh@shaw.ca>
2016-05-05 03:03:43 -0700

    Use argparse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import gi
 
 gi.require_version('Gtk', '3.0')
@@ -7,6 +7,15 @@ gi.require_version('WebKit2', '4.0')
 from ui import Window
 from parse import EPub
 
+def main():
+    parser = argparse.ArgumentParser(prog='epubmangler', description='Modify epub metadata.')
+    parser.add_argument('file', metavar='FILE', help='the path to an epub file')
+    args = parser.parse_args()
 
-win = Window(EPub(sys.argv[1], debug=True))
-gi.repository.Gtk.main()
+    if args.file:
+        Window(EPub(args.file, debug=True))
+        gi.repository.Gtk.main()
+
+if __name__ == '__main__':
+    main()
+    
