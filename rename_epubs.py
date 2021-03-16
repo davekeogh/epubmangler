51662748b83c5f9d3be814f8b0af7ed1deeba466
David Keogh <davidtkeogh@gmail.com>
2021-03-16 14:10:52 -0700

    Use timeit in rename_epubs

--- rename_epubs.py
+++ rename_epubs.py
@@ -4,8 +4,32 @@
 import os
 import os.path
 import sys
+import timeit
 
 from epubmangler import EPub, is_epub, ILLEGAL_CHARS
+
+
+def rename_file(path: str) -> None:
+    book = EPub(full_path)
+            
+    try:
+        title = book.get('title').text
+        author = book.get('creator').text
+    except NameError:
+        print(f'No title or author metadata: {file}')
+        return
+
+    file_name = f'{author} - {title}.epub'
+
+    # Some characters are not allowed in filenames especially on Windows
+    for char in ILLEGAL_CHARS:
+        file_name = file_name.replace(char, '-')
+
+    new_file = os.path.join(DIR, file_name)
+
+    if new_file != full_path:
+        os.rename(full_path, new_file)
+
 
 if __name__ == '__main__':
 
@@ -13,27 +37,15 @@ if __name__ == '__main__':
         DIR = sys.argv[1]
     else:
         DIR = os.getcwd()
+    
+    FILES = 0
+    TIME = 0
 
     for file in os.listdir(DIR):
         full_path = os.path.join(DIR, file)
 
         if is_epub(full_path):
-            book = EPub(full_path)
-            
-            try:
-                title = book.get('title').text
-                author = book.get('creator').text
-            except NameError:
-                print(f'No title or author metadata: {file}')
-                continue
+            FILES += 1
+            TIME += timeit.timeit(stmt='rename_file(full_path)', setup='from __main__ import rename_file, full_path', number=1)
 
-            file_name = f'{author} - {title}.epub'
-
-            # Some characters are not allowed in filenames especially on Windows
-            for char in ILLEGAL_CHARS:
-                file_name = file_name.replace(char, '-')
-
-            new_file = os.path.join(DIR, file_name)
-
-            if new_file != full_path:
-                os.rename(full_path, new_file)
+    print(f'Read {FILES} files in {TIME} seconds')
