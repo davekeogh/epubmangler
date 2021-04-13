885f2869aafc5b1931fcf1c3c461f0d20da3e2b1
David Keogh <davidtkeogh@gmail.com>
2021-04-12 22:25:51 -0700

    code formatting

--- edit.py
+++ edit.py
@@ -5,7 +5,7 @@
 # - look into using libhandy widgets, they will be added to Gtk 4 with libadwaita
 # - Use Gtk.Application, Gtk.ApplicationWindow
 
-import mimetypes, os, os.path, random, shutil, sys
+import mimetypes, os, os.path, random, sys
 
 from epubmangler import EPub, IMAGE_TYPES, is_epub, strip_namespace, strip_namespaces
 
@@ -18,20 +18,20 @@ from gi.repository import Gdk, GdkPixbuf, Gio, Gtk
 def send_book(button: Gtk.Button, book: EPub, device_path: str, window: Gtk.Window) -> None:
     file_name = os.path.basename(book.file)
     copy_to = os.path.join(device_path, file_name)
-    
+
     if not os.path.exists(copy_to):
         book.save(copy_to)
-    
+
     else:
         dialog = Gtk.MessageDialog(text='File already exists',
                                    message_type=Gtk.MessageType.QUESTION)
         dialog.format_secondary_text(f'Replace file "{file_name}"?')
         dialog.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
                            Gtk.STOCK_SAVE, Gtk.ResponseType.OK)
-        
+
         if dialog.run() == Gtk.ResponseType.OK:
             book.save(copy_to, overwrite=True)
-        
+
         dialog.destroy()
 
 
@@ -41,7 +41,8 @@ def scale_cover(file: str, allocation: Gdk.Rectangle) -> GdkPixbuf.Pixbuf:
     return GdkPixbuf.Pixbuf.new_from_file_at_scale(file, width, height, True)
 
 
-def cell_edited(_c: Gtk.CellRendererText, path: str, new_text: str, model: Gtk.ListStore, col: int) -> None:
+def cell_edited(_c: Gtk.CellRendererText,
+                path: str, new_text: str, model: Gtk.ListStore, col: int) -> None:
     model[path][col] = new_text
 
 
@@ -59,8 +60,9 @@ def save_file(_b: Gtk.Button, book: EPub, window: Gtk.Window) -> None:
     dialog.destroy()
 
 
-def details_toggle(b: Gtk.ToggleButton, cover: Gtk.Button, main: Gtk.Grid, details: Gtk.TreeView) -> None:
-    details_on = b.get_active()
+def details_toggle(button: Gtk.ToggleButton,
+                   cover: Gtk.Button, main: Gtk.Grid, details: Gtk.TreeView) -> None:
+    details_on = button.get_active()
     details.set_visible(details_on)
     main.set_visible(not details_on)
     cover.set_visible(not details_on)
@@ -161,19 +163,19 @@ if __name__ == '__main__':
     details_model = Gtk.ListStore(str, str, str)
 
     volume_monitor = Gio.VolumeMonitor.get()
-    
+
     for drive in volume_monitor.get_connected_drives():
         if drive.get_name() == 'Kindle Internal Storage':
             mount = drive.get_volumes()[0].get_mount()
 
             if mount:
                 root = os.path.join(mount.get_root().get_path(), 'documents')
-    
+
                 if os.path.exists(root):
                     device_button.connect('clicked', send_book, book, root, window)
                     device_button.set_label('Send to Kindle')
                     device_button.show()
-    
+
     # Signals
     window.connect('destroy', Gtk.main_quit)
     save_button.connect('clicked', save_file, book, window)
@@ -202,7 +204,7 @@ if __name__ == '__main__':
 
         for subject in book.get_all('subject'):
             list_model.append([subject.text])
-        
+
         subject_view.set_model(list_model)
         subject_view.append_column(Gtk.TreeViewColumn('Subjects', Gtk.CellRendererText(), text=0))
 
@@ -246,13 +248,11 @@ if __name__ == '__main__':
             buffer = Gtk.TextBuffer()
             buffer.set_text(description_text)
 
-            buffer.connect(
-                'changed',
-                lambda buffer, book: 
-                    book.set('description', buffer.get_text(buffer.get_start_iter(),
-                             buffer.get_end_iter(), True)),
-                book
-            )
+            buffer.connect('changed',
+                           lambda buffer, book:
+                           book.set('description', buffer.get_text(buffer.get_start_iter(),
+                                                                   buffer.get_end_iter(), True)),
+                           book)
 
             description.set_buffer(buffer)
 
