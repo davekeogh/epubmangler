4a66a998a3fa76c693085597b8bc33506a7b2079
David Keogh <davidtkeogh@gmail.com>
2021-04-13 20:14:19 -0700

    example: demonstrate all methods

--- example/example.py
+++ example/example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Example usage of epubmangler to edit the metadata of an ebook."""
 
+import time
 from epubmangler import EPub
 
 with EPub('Frankenstein.epub') as book: # https://gutenberg.org/ebooks/84
@@ -8,17 +9,17 @@ with EPub('Frankenstein.epub') as book: # https://gutenberg.org/ebooks/84
     # Get information about a book
     language = book.get('language')
     subjects = book.get_all('subject')
+    elements = book.metadata()
 
     # Modify existing elements
     book.set('title', 'Frankenstein 2')
-
-    # Add and remove elements
-    book.add('creator', 'David Keogh', {'opf:role' : 'aut'})
-    book.remove('date', {'opf:event' : 'conversion'})
-
-    # Convenience functions
     book.set_cover('cat_picture.jpg')
     book.set_identifier('http://github.com/davekeogh/epubmangler', 'URI')
+
+    # Add and remove elements
+    book.add('contributor', 'epubmangler', {'opf:role' : 'bkp'})
+    book.remove('date', {'opf:event' : 'conversion'})
+    book.add('date', time.strftime('%F'), {'opf:event' : 'conversion'})
 
     # Add and remove subjects
     book.add_subject('Sequel')
