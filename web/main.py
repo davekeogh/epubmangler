ed99316e4ae3067a085b7373a70e3bcf8eb42a28
David Keogh <davidtkeogh@gmail.com>
2021-05-19 22:55:26 -0700

    web: redirect to "/" on error

--- web/main.py
+++ web/main.py
@@ -1,11 +1,15 @@
+import os
 import pathlib
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, status
 from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 
 from epubmangler import EPub
 
 app = FastAPI()
+
+# TODO: Needs a backend to delete everything in upload after a while
+
 
 @app.get("/")
 async def main():
@@ -14,21 +18,24 @@ async def main():
 
     return HTMLResponse("upload a book")
 
+
 @app.post("/edit")
 async def edit(file: UploadFile = File(...)):
     filename = pathlib.Path('upload', file.filename)
-    
+
     with open(filename, 'wb') as temp:
         temp.write(await file.read())
-    
+
     try:
         epub = EPub(filename)
     except ValueError:
-        return RedirectResponse("/")
-    
+        os.remove(filename)
+        return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
+
     # TODO: This is where everything happens
-    
-    return HTMLResponse(f"editing: {epub.opf}")
+
+    return HTMLResponse(f"editing: {epub.file}")
+
 
 @app.put("/download")
 async def download(filename: str):
