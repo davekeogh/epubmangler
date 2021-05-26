72abdb5cd1158c40e9e6b396ca32969af0a13f6c
David Keogh <davidtkeogh@gmail.com>
2021-05-25 22:16:35 -0700

    web: basic edit form and refactor

--- web/main.py
+++ web/main.py
@@ -1,13 +1,14 @@
 """A simple web application using epubmangler"""
 
 import os
+import shutil
 from pathlib import Path
 
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
-from epubmangler import EPub
+from epubmangler import EPub, strip_namespace
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -34,11 +35,37 @@ async def edit(file: UploadFile = File(...)):
         os.remove(filename)
         return HTMLResponse(template.replace("{{ body }}", f"not an epub: {filename}"))
 
-    inner_html = f"""editing: {epub.file}
-    <form action="/download" method="POST">
-    <input type="hidden" name="filename" value="{filename}" />
-    <button type="submit">Save</button>
-    </form>"""
+    cover_path = Path(epub.get_cover())
+    temp_cover = None
+
+    if cover_path.is_file():
+        temp_cover = Path('static', f'{hash(str(cover_path))}.{cover_path.suffix}')
+        shutil.copy(cover_path, temp_cover)
+
+    inner_html = (
+        f'<p>editing: {epub.file}</p>\n'
+        '<form action="/download" method="POST">\n'
+        f'<input type="hidden" name="filename" value="{filename}" />\n'
+    )
+
+    for item in epub.metadata:
+        inner_html += (
+            f'<label for="{item.tag}">{strip_namespace(item.tag)}:</label>'
+            f'<input type="text" name="{item.tag}-text" value="{item.text}" />'
+        )
+
+        if item.attrib:
+            inner_html += f'<input type="text" name="{item.tag}-attrib" value="{item.attrib}" />'
+
+        inner_html += f'<button type="button" class="delete">x</button><br />\n'
+
+    inner_html += (
+        '<label for="new-tag">new:</label><input type="text" name="new-tag" />'
+        '<input type="text" name="new-text" /><input type="text" name="new-attrib" />'
+        '<button type="button" class="add">+</button><br /><br />\n'
+        '<button type="submit">Save</button>\n'
+        '</form>'
+    )
 
     return HTMLResponse(template.replace("{{ body }}", inner_html))
 
