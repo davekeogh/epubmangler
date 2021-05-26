05a2abdd4d56d46819ec7f1145572b2875e6d3d0
David Keogh <davidtkeogh@gmail.com>
2021-05-25 20:07:30 -0700

    web: skeleton of a fastapi app

--- web/main.py
+++ web/main.py
@@ -1,44 +1,53 @@
 """A simple web application using epubmangler"""
 
 import os
-import pathlib
+from pathlib import Path
 
-from fastapi import FastAPI, File, UploadFile, status
-from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from epubmangler import EPub
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+template = open(Path("static/template.html"), "r").read()
 
 # TODO: Needs a backend to delete everything in /upload after a while
 
 
 @app.get("/")
 async def main():
-
-    # TODO: Upload a file here
-
-    return HTMLResponse("upload a book")
+    return HTMLResponse(open(Path("static/main.html"), "r").read())
 
 
 @app.post("/edit")
 async def edit(file: UploadFile = File(...)):
-    filename = pathlib.Path('upload', file.filename)
+    filename = Path("upload", file.filename)
 
-    with open(filename, 'wb') as temp:
+    with open(filename, "wb") as temp:
         temp.write(await file.read())
 
     try:
         epub = EPub(filename)
     except ValueError:
         os.remove(filename)
-        return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
+        return HTMLResponse(template.replace("{{ body }}", f"not an epub: {filename}"))
 
-    # TODO: This is where everything happens
+    inner_html = f"""editing: {epub.file}
+    <form action="/download" method="POST">
+    <input type="hidden" name="filename" value="{filename}" />
+    <button type="submit">Save</button>
+    </form>"""
 
-    return HTMLResponse(f"editing: {epub.file}")
+    return HTMLResponse(template.replace("{{ body }}", inner_html))
 
 
-@app.put("/download")
-async def download(filename: str):
-    return FileResponse(filename)
+@app.post("/download")
+async def download(filename: str = Form(...)):
+    if Path(filename).parent != Path("upload"):
+        return HTMLResponse(template.replace("{{ body }}", f"bad request: {filename}"))
+
+    inner_html = f"downloading: {filename}"
+
+    return HTMLResponse(template.replace("{{ body }}", inner_html))
